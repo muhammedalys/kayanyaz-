e7d1f704aa8aa894dbf0a48abdfcf597c7146bc7
muhammedalys <muhammed_aly_ozen@hotmail.com>
2017-11-09 23:52:30 -0800

    Add files via upload

---
+++ sketch_nov10a.ino
@@ -0,0 +1,21 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,5,4,3,2); //LCD nin arduino üzerinde bağlandığı pinler
+
+void setup(){
+lcd.begin(16,2);
+}
+
+void loop(){
+int i;
+lcd.setCursor(3,0);
+lcd.print("Arduino Bey"); // İstediğinizi yazabilirsiniz. Tabi 16 karakter den küçük olmalı…
+for(i=0; i<17; i++)
+{
+lcd.setCursor(i,1);
+lcd.print("Muhammed Ali"); // İstediğinizi yazabilirsiniz. Tabi 16 karakter den küçük olmalı…
+delay(1000);
+lcd.setCursor(i,1);
+lcd.print(" ");
+}
+}
